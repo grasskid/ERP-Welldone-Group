5e71bcecc0655fa362a0c5d4a16c115467d44944
Andis Trihariprasetya <andissetya@gmail.com>
2025-07-22 18:00:20 +0700

    update

--- public/template/assets/js/apps/kanban.js
+++ public/template/assets/js/apps/kanban.js
@@ -1,27 +1,17 @@
 $(function () {
-  // ----------------------------------------------------------------------
-  // draggble item
-  // ----------------------------------------------------------------------
   function kanbanSortable() {
-    $('[data-sortable="true"]').sortable({
-      connectWith: ".connect-sorting-content",
-      items: ".card",
-      cursor: "move",
-      placeholder: "ui-state-highlight",
-      refreshPosition: true,
-      stop: function (event, ui) {
-        var parent_ui = ui.item.parent().attr("data-item");
-      },
-      update: function (event, ui) {
-        console.log(ui);
-        console.log(ui.item);
-      },
+    document.querySelectorAll('[data-sortable="true"]').forEach((el) => {
+      new Sortable(el, {
+        group: 'kanban-tasks',
+        animation: 150,
+        ghostClass: 'ui-state-highlight',
+        onEnd: function (evt) {
+          console.log('Moved item:', evt.item);
+        }
+      });
     });
   }
 
-  // ----------------------------------------------------------------------
-  // clear all task on click
-  // ----------------------------------------------------------------------
   function clearItem() {
     $(".list-clear-all")
       .off("click")
@@ -34,120 +24,60 @@ $(function () {
       });
   }
 
-  // ----------------------------------------------------------------------
-  // add task and open modal
-  // ----------------------------------------------------------------------
   function addKanbanItem() {
-    $(".addTask").on("click", function (event) {
-      event.preventDefault();
-      getParentElement = $(this)
-        .parents('[data-action="sorting"]')
-        .attr("data-item");
-      $(".edit-task-title").hide();
-      $(".add-task-title").show();
-      $('[data-btn-action="addTask"]').show();
-      $('[data-btn-action="editTask"]').hide();
-      $("#addItemModal").modal("show");
-      kanban_add(getParentElement);
-    });
-  }
+  $(".addTask").on("click", function (event) {
+    event.preventDefault();
+    const getParentElement = $(this).parents('[data-action="sorting"]').attr("data-item");
 
-  // ----------------------------------------------------------------------
-  //   add default item
-  // ----------------------------------------------------------------------
-  function kanban_add(getParent) {
-    $('[data-btn-action="addTask"]')
-      .off("click")
-      .on("click", function (event) {
-        getAddBtnClass = $(this).attr("class").split(" ")[1];
+    $(".edit-task-title").hide();
+    $(".add-task-title").show();
+    $('[data-btn-action="addTask"]').hide();
+    $('[data-btn-action="editTask"]').hide();
 
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, "0");
-        var mm = String(today.getMonth());
+    $("#addItemModal .modal-body").html(`
+      <form id="kanban-form" enctype="multipart/form-data" method="post">
+        <div class="mb-3">
+          <label class="form-label">Nama Tugas</label>
+          <input type="text" class="form-control" name="nama_tugas" required />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Deskripsi</label>
+          <textarea class="form-control" name="deskripsi" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Upload File</label>
+          <input type="file" class="form-control" name="file" accept=".jpg,.jpeg,.png,.pdf" />
+        </div>
+        <input type="hidden" name="status" value="1" />
+        <button type="submit" class="btn btn-primary">Save</button>
+      </form>
+    `);
 
-        var monthNames = [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
-        ];
+    $("#addItemModal").modal("show");
 
-        today = dd + " " + monthNames[mm];
+    kanban_add();
+  });
+}
 
-        var $_getParent = getParent;
+function kanban_add() {
+  $(document).off("submit", "#kanban-form").on("submit", "#kanban-form", function (e) {
+    e.preventDefault();
 
-        var itemTitle = document.getElementById("kanban-title").value;
-        var itemText = document.getElementById("kanban-text").value;
+    const form = document.getElementById("kanban-form");
+    const formData = new FormData(form);
 
-        item_html =
-          '<div data-draggable="true" class="card task-text-progress" style="">' +
-          '<div class="card-body">' +
-          '<div class="task-header">' +
-          '<div class="">' +
-          '<h4 class="" data-item-title="' +
-          itemTitle +
-          '">' +
-          itemTitle +
-          "</h4>" +
-          "</div>" +
-          '<div class="">' +
-          '<div class="dropdown">' +
-          '<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink-4" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
-          '<i class="ti ti-dots-vertical text-dark"></i>' +
-          "</a>" +
-          '<div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink-4">' +
-          '<a class="dropdown-item kanban-item-edit cursor-pointer d-flex align-items-center gap-1" href="javascript:void(0);"><i class="ti ti-pencil fs-5"></i>Edit</a>' +
-          '<a class="dropdown-item kanban-item-delete cursor-pointer d-flex align-items-center gap-1" href="javascript:void(0);"><i class="ti ti-trash fs-5"></i>Delete</a>' +
-          "</div>" +
-          "</div>" +
-          "</div>" +
-          "</div>" +
-          '<div class="task-body">' +
-          '<div class="task-content">' +
-          '<p class="mb-0" data-item-text="' +
-          itemText +
-          '">' +
-          itemText +
-          "</p>" +
-          "</div>" +
-          '<div class="task-bottom">' +
-          '<div class="tb-section-1">' +
-          '<span class="hstack gap-2 fs-2" data-item-date="' +
-          today +
-          '"><i class="ti ti-calendar fs-5"></i> ' +
-          today +
-          "</span>" +
-          "</div>" +
-          '<div class="tb-section-2">' +
-          '<span class="badge text-bg-success fs-1">Design</span>' +
-          "</div>" +
-          "</div>" +
-          "</div>" +
-          "</div>" +
-          "</div>";
-
-        $("[data-item='" + $_getParent + "'] .connect-sorting-content").append(
-          item_html
-        );
-
-        $("#addItemModal").modal("hide");
-
-        kanbanEdit();
-        kanbanDelete();
+    fetch("/tugas/saveTugas", {
+      method: "POST",
+      body: formData
+    })
+      .then(res => res.ok ? location.reload() : alert("Gagal menambahkan tugas."))
+      .catch(err => {
+        console.error("Upload error:", err);
+        alert("Terjadi kesalahan saat mengirim tugas.");
       });
-  }
+  });
+}
 
-  // ----------------------------------------------------------------------
-  //   add item
-  // ----------------------------------------------------------------------
   $("#add-list")
     .off("click")
     .on("click", function (event) {
@@ -160,9 +90,6 @@ $(function () {
       $("#addListModal").modal("show");
     });
 
-  // ----------------------------------------------------------------------
-  //   add list
-  // ----------------------------------------------------------------------
   $(".add-list")
     .off("click")
     .on("click", function (event) {
@@ -173,43 +100,40 @@ $(function () {
       today = mm + "." + dd;
 
       var itemTitle = document.getElementById("item-name").value;
-
       var itemNameLowercase = itemTitle.toLowerCase();
       var itemNameRemoveWhiteSpace = itemNameLowercase.split(" ").join("_");
       var itemDataAttr = itemNameRemoveWhiteSpace;
 
-      item_html =
-        '<div data-item="item-' +
-        itemDataAttr +
-        '" class="task-list-container  mb-4 " data-action="sorting">' +
-        '<div class="connect-sorting">' +
-        '<div class="task-container-header">' +
-        '<h6 class="item-head mb-0 fs-4 fw-semibold" data-item-title="' +
-        itemTitle +
-        '">' +
-        itemTitle +
-        "</h6>" +
-        '<div class="hstack gap-2">' +
-        '<div class="dropdown">' +
-        '<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink-4" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
-        '<i class="ti ti-dots-vertical text-dark"></i>' +
-        "</a>" +
-        '<div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink-4">' +
-        '<a class="dropdown-item list-edit" href="javascript:void(0);">Edit</a>' +
-        '<a class="dropdown-item list-delete" href="javascript:void(0);">Delete</a>' +
-        '<a class="dropdown-item list-clear-all" href="javascript:void(0);">Clear All</a>' +
-        "</div>" +
-        "</div>" +
-        "</div>" +
-        "</div>" +
-        '<div class="connect-sorting-content" data-sortable="true">' +
-        "</div>" +
-        "</div>" +
-        "</div>";
+      var item_html = `
+        <div data-item="item-${itemDataAttr}" class="task-list-container mb-4" data-action="sorting">
+          <div class="connect-sorting">
+            <div class="task-container-header d-flex justify-content-between">
+              <h6 class="item-head mb-0 fs-4 fw-semibold" data-item-title="${itemTitle}">${itemTitle}</h6>
+              <div class="hstack gap-2">
+                <div class="dropdown">
+                  <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                    <i class="ti ti-dots-vertical text-dark"></i>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-end">
+                    <a class="dropdown-item list-edit" href="javascript:void(0);">Edit</a>
+                    <a class="dropdown-item list-delete" href="javascript:void(0);">Delete</a>
+                    <a class="dropdown-item list-clear-all" href="javascript:void(0);">Clear All</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="connect-sorting-content" data-sortable="true"></div>
+            <div class="text-center mt-2">
+              <button class="btn btn-sm btn-outline-primary addTask">+ Add Task</button>
+            </div>
+          </div>
+        </div>
+      `;
 
       $(".task-list-section").append(item_html);
       $("#addListModal").modal("hide");
       $("#item-name").val("");
+
       kanbanSortable();
       editItem();
       deleteItem();
@@ -218,20 +142,12 @@ $(function () {
       kanbanEdit();
       kanbanDelete();
 
-      // --------------------
-      // Tooltip
-      // --------------------
-      var tooltipTriggerList = [].slice.call(
-        document.querySelectorAll('[data-bs-toggle="tooltip"]')
-      );
-      var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+      tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
       });
     });
 
-  // ----------------------------------------------------------------------
-  // edit item
-  // ----------------------------------------------------------------------
   function editItem() {
     $(".list-edit")
       .off("click")
@@ -242,44 +158,25 @@ $(function () {
 
         $(".add-list").hide();
         $(".edit-list").show();
-
         $(".add-list-title").hide();
         $(".edit-list-title").show();
 
-        var itemTitle = parentItem
-          .parents('[data-action="sorting"]')
-          .find(".item-head")
-          .attr("data-item-title");
+        var itemTitle = parentItem.parents('[data-action="sorting"]').find(".item-head").attr("data-item-title");
         $("#item-name").val(itemTitle);
 
         $(".edit-list")
           .off("click")
-          .on("click", function (event) {
-            var $_innerThis = $(this);
-            var $_getListTitle = document.getElementById("item-name").value;
-
-            var $_editedListTitle = parentItem
-              .parents('[data-action="sorting"]')
-              .find(".item-head")
-              .html($_getListTitle);
-            var $_editedListTitleDataAttr = parentItem
-              .parents('[data-action="sorting"]')
-              .find(".item-head")
-              .attr("data-item-title", $_getListTitle);
-
+          .on("click", function () {
+            var newTitle = $("#item-name").val();
+            parentItem.parents('[data-action="sorting"]').find(".item-head").text(newTitle).attr("data-item-title", newTitle);
             $("#addListModal").modal("hide");
             $("#item-name").val("");
           });
+
         $("#addListModal").modal("show");
-        $("#addListModal").on("hidden.bs.modal", function (e) {
-          $("#item-name").val("");
-        });
       });
   }
 
-  // ----------------------------------------------------------------------
-  // all list delete
-  // ----------------------------------------------------------------------
   function deleteItem() {
     $(".list-delete")
       .off("click")
@@ -289,92 +186,67 @@ $(function () {
       });
   }
 
-  // ----------------------------------------------------------------------
-  // Delete item on click
-  // ----------------------------------------------------------------------
   function kanbanDelete() {
     $(".card .kanban-item-delete")
       .off("click")
       .on("click", function (event) {
         event.preventDefault();
-
-        get_card_parent = $(this).parents(".card");
-
+        var get_card_parent = $(this).parents(".card");
         $("#deleteConformation").modal("show");
 
-        $('[data-remove="task"]').on("click", function (event) {
+        $('[data-remove="task"]').off("click").on("click", function (event) {
           event.preventDefault();
-          /* Act on the event */
           get_card_parent.remove();
           $("#deleteConformation").modal("hide");
         });
       });
   }
 
-  // ----------------------------------------------------------------------
-  // Edit item on click
-  // ----------------------------------------------------------------------
   function kanbanEdit() {
     $(".card .kanban-item-edit")
       .off("click")
       .on("click", function (event) {
         event.preventDefault();
-
         var parentItem = $(this);
 
         $(".add-task-title").hide();
         $(".edit-task-title").show();
-
         $('[data-btn-action="addTask"]').hide();
         $('[data-btn-action="editTask"]').show();
 
-        var itemKanbanTitle = parentItem
-          .parents(".card")
-          .find("h4")
-          .attr("data-item-title");
-        var get_kanban_title = $(".task-text-progress #kanban-title").val(
-          itemKanbanTitle
-        );
+        var itemKanbanTitle = parentItem.parents(".card").find("h4").attr("data-item-title");
+        var itemText = parentItem.parents(".card").find("p").attr("data-item-text");
 
-        var itemText = parentItem
-          .parents(".card")
-          .find('p:not(".progress-count")')
-          .attr("data-item-text");
-        var get_kanban_text = $(".task-text-progress #kanban-text").val(
-          itemText
-        );
+        $("#addItemModal .modal-body").html(`
+          <form id="kanban-form">
+            <div class="mb-3">
+              <label for="kanban-title" class="form-label">Title</label>
+              <input type="text" class="form-control" id="kanban-title" value="${itemKanbanTitle}" />
+            </div>
+            <div class="mb-3">
+              <label for="kanban-text" class="form-label">Description</label>
+              <textarea class="form-control" id="kanban-text" rows="3">${itemText}</textarea>
+            </div>
+          </form>
+        `);
 
         $('[data-btn-action="editTask"]')
           .off("click")
-          .on("click", function (event) {
-            var kanbanValueTitle =
-              document.getElementById("kanban-title").value;
-            var kanbanValueText = document.getElementById("kanban-text").value;
-
-            var itemDataAttr = parentItem
-              .parents(".card")
-              .find("h4")
-              .attr("data-item-title", kanbanValueTitle);
-            var itemKanbanTitle = parentItem
-              .parents(".card")
-              .find("h4")
-              .html(kanbanValueTitle);
-            var itemTextDataAttr = parentItem
-              .parents(".card")
-              .find('p:not(".progress-count")')
-              .attr("data-tasktext", kanbanValueText);
-            var itemText = parentItem
-              .parents(".card")
-              .find('p:not(".progress-count")')
-              .html(kanbanValueText);
-
+          .on("click", function () {
+            var newTitle = $("#kanban-title").val();
+            var newText = $("#kanban-text").val();
+            parentItem.parents(".card").find("h4").text(newTitle).attr("data-item-title", newTitle);
+            parentItem.parents(".card").find("p").text(newText).attr("data-item-text", newText);
             $("#addItemModal").modal("hide");
-            var setDate = $(".taskDate").html("");
-            $(".taskDate").hide();
           });
+
         $("#addItemModal").modal("show");
       });
   }
+
+  $("#addItemModal").on("hidden.bs.modal", function () {
+    $("#kanban-form")[0]?.reset();
+  });
 
   editItem();
   deleteItem();
@@ -383,4 +255,58 @@ $(function () {
   kanbanEdit();
   kanbanDelete();
   kanbanSortable();
+
+  document.querySelectorAll('.connect-sorting-content').forEach(list => {
+    new Sortable(list, {
+        group: 'kanban',
+        animation: 150,
+        onAdd: function (evt) {
+            const card = evt.item;
+            const targetColumn = evt.to.closest('.task-list-container').getAttribute('data-item');
+            const dropdown = card.querySelector('.dropdown-toggle');
+
+            if (!dropdown) return;
+
+            const idtugas = dropdown.id.replace('dropdownMenuLink-', '');
+
+            // Remove any previous confirm buttons
+            card.querySelectorAll('.confirm-status-btn')?.forEach(el => el.remove());
+
+            const confirmBtn = document.createElement('button');
+confirmBtn.innerText = '✔ Confirm';
+confirmBtn.className = 'btn btn-sm btn-success mb-2 ms-2 confirm-status-btn';
+card.querySelector('.card-body').appendChild(confirmBtn);
+
+            let newStatus = 1;
+            if (targetColumn === 'item-inprogress') newStatus = 2;
+            else if (targetColumn === 'item-pending') newStatus = 3;
+            else if (targetColumn === 'item-done') newStatus = 4;
+
+            confirmBtn.addEventListener('click', function () {
+                fetch('/tugas/updateStatus', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Requested-With': 'XMLHttpRequest'
+                    },
+                    body: JSON.stringify({ id: idtugas, status: newStatus })
+                })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.success) {
+                        confirmBtn.remove();
+                    } else {
+                        alert('Failed to update status.');
+                    }
+                })
+                .catch(err => {
+                    console.error('AJAX error:', err);
+                    alert('AJAX request failed.');
+                });
+            });
+        }
+    });
+});
+
+
 });
